c9acf6a200dec4f0aa71278aebd95b68e4d22db1
axelviv <axelviv@live.com.ar>
2024-10-04 13:24:03 -0300

    navbar y footer insertados por JS

---
+++ js/modules/footer.js
@@ -0,0 +1,9 @@
+
+
+export const footerModule = `
+
+    <div class="bg-dark text-secondary text-center py-3">
+                <h2 class="fs-6"> © 1990 - 2024 Accessible Travel. Todos los derechos reservados.</h2>
+    </div>
+
+    `
--- js/pages/home.js
+++ js/pages/home.js
@@ -1,6 +1,8 @@
 import { brandsSlider } from "../modules/brands-slider.js";
 import sliderLatestNew from "../modules/latest-new.js";
 import { fetchData } from "../modules/fetch-data.js";
+import { navBarModule } from "../modules/navBar.js"
+import { footerModule } from "../modules/footer.js"
 
 const loadHomePage = async () => {
   const dbInfo = await fetchData();
@@ -10,3 +12,18 @@ const loadHomePage = async () => {
 };
 
 loadHomePage();
+
+
+/* NavBar */
+let navContainer = document.querySelector('header')
+
+window.addEventListener('load', ()=>{
+    navContainer.innerHTML = navBarModule
+})
+
+/* Footer */
+let footerContainer = document.querySelector('footer')
+
+window.addEventListener('load', ()=>{
+    footerContainer.innerHTML = footerModule
+})
